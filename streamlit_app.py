b30c9b70f2147648850db3eadd16c1d4a27f42aa
Jayz2000 <41512657+Jayz2000@users.noreply.github.com>
2024-04-12 12:24:34 -0400

    Update streamlit_app.py
    search_on needs to be a str

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.') # this print out the fruit_name and fruit_search_on
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.') # this print out the fruit_name and fruit_search_on
 
         st.subheader(fruit_chosen + 'Nutrition information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + str(search_on))
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
     # st.write(ingredients_string)
